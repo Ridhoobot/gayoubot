3cdacd5bd59711da96f60d71aff8bf7ccf7f98fb
JI CHANG WOOK <106302889+Ridhoobot@users.noreply.github.com>
2024-11-02 01:11:12 +0800

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -13,7 +13,7 @@ from pyrogram.types import Message
 from pytgcalls import PyTgCalls
 from pytgcalls import filters as fl
 from pyromod import listen
-from PyroUbot.config import *
+from PyroUbot import 
 from aiohttp import ClientSession
 
 class ConnectionHandler(logging.Handler):
